adf1f7a0dba7b5e2dec2f15d8f885c699152bd33
Jason Barr <jason.barr@metronet.com>
2022-12-12 15:59:38 -0600

    Stub out shop and navigation components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
+import Home, { Navigation } from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route path="/home" element={<Home />}>
+          <Route path="/shop" element={<Shop />} />
+        </Route>
+      </Route>
     </Routes>
   );
 };
--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -1,8 +1,25 @@
+import { Outlet } from "react-router-dom";
 import categories from "../../data/categories";
 import Directory from "../../components/directory/directory.component";
 
+export const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const Home = () => {
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} />;
+      <Outlet />
+    </div>
+  );
 };
 
 export default Home;
