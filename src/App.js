63e3eca1b9a963144555bc55b9f6cd18f7cdadfa
Jason Barr <jason.barr@metronet.com>
2022-12-12 16:18:03 -0600

    Link to shop page and style nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home, { Navigation } from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="/home" element={<Home />}>
-          <Route path="/shop" element={<Shop />} />
-        </Route>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
   );
--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -2,17 +2,6 @@ import { Outlet } from "react-router-dom";
 import categories from "../../data/categories";
 import Directory from "../../components/directory/directory.component";
 
-export const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
-
 const Home = () => {
   return (
     <div>
