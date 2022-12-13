b51147c15482f33e6923db6bdee3bbc7aefab27b
Jason Barr <jason.barr@metronet.com>
2022-12-13 04:49:01 -0600

    Clean up link

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -11,7 +11,7 @@ const Navigation = () => {
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
-            SHOP{" "}
+            SHOP
           </Link>
         </div>
       </div>
