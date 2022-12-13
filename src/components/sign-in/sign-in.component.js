fba807a2b7aa99fa9873e9b8606fcb11bc97f117
Jason Barr <jason.barr@metronet.com>
2022-12-13 05:37:06 -0600

    Authenticate with Google

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,7 +1,18 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    createUserDocumentFromAuth(response);
+  };
+
   return (
     <div>
       <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
     </div>
   );
 };
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,14 +1,36 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP.apiKey,
-  authDomain: process.env.REACT_APP.authDomain,
-  projectId: process.env.REACT_APP.projectId,
-  storageBucket: process.env.REACT_APP.storageBucket,
-  messagingSenderId: process.env.REACT_APP.messagingSenderId,
-  appId: process.env.REACT_APP.appId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.user.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+};
